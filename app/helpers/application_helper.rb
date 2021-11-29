86279de80266a0162dec9604a1c8f2c53af80cda
Syu <konjikicity@gmail.com>
2021-11-29 23:38:09 +0900

    Improve: erbからslimへ変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
 
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
+
+  def flash_class_for(key)
+    case key
+      when 'success' then 'alert-success'
+      when 'failed'   then 'alert-danger'
+    end
+  end
 end
